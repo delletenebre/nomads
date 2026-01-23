2d1e7b5c8f1ec481f80231762d35a7d34e194874
Sergei Nezhinskikh <delletenebre@gmail.com>
2026-01-23 23:52:40 +0600

    TweenAnimationBuilder

--- lib/ui/game_cards/game_card_placeholder.dart
+++ lib/ui/game_cards/game_card_placeholder.dart
@@ -14,31 +14,39 @@ class GameCardPlaceholder extends HookWidget {
   @override
   Widget build(BuildContext context) {
     // Use an AnimationController with `useAnimationController` hook
-    final controller = useAnimationController(
-      duration: const Duration(milliseconds: 200),
-    );
+    // final controller = useAnimationController(
+    //   duration: const Duration(milliseconds: 200),
+    // );
 
-    final animation = useMemoized(() {
-      return Tween<double>(begin: 0.0, end: 100.0).animate(controller);
-    }, [controller]);
+    // final animation = useMemoized(() {
+    //   return Tween<double>(begin: 0.0, end: 100.0).animate(controller);
+    // }, [controller]);
 
-    useEffect(() {
-      controller.forward();
-      return null;
-    }, const []);
+    // useEffect(() {
+    //   controller.forward();
+    //   return null;
+    // }, const []);
 
-    final animatedWidth = useAnimation(animation);
+    // final animatedWidth = useAnimation(animation);
 
     // Это просто стилизованный контейнер, который показывает,
     // куда можно поместить карту.
-    return Container(
-      width: animatedWidth,
-      height: 150, // Высота должна совпадать с высотой карты (150.0)
-      margin: EdgeInsets.symmetric(horizontal: spacing / 2),
-      decoration: BoxDecoration(
-        color: Colors.black.withOpacity(0.1),
-        borderRadius: BorderRadius.circular(8.0),
-      ),
+    return TweenAnimationBuilder<double>(
+      tween: Tween<double>(begin: 0, end: 100),
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
+      builder: (context, width, child) {
+        print(width);
+        return Container(
+          width: width,
+          height: 150, // Высота должна совпадать с высотой карты (150.0)
+          margin: EdgeInsets.symmetric(horizontal: spacing / 2),
+          decoration: BoxDecoration(
+            color: Colors.black.withOpacity(0.1),
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+        );
+      },
     );
   }
 }
